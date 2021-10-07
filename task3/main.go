7dcedfc21192d5c8271538fabc6c04b44057701a
useralexey2000 <useralexey2000@gmail.com>
2021-10-07 16:19:18 +0300

    Refactoring fib() func.

--- task3/main.go
+++ task3/main.go
@@ -13,15 +13,10 @@ func main() {
 // 0, 1, 1, 2, 3, 5
 // Task3
 func fib() func() int {
-	n := 0
-	arr := []int{0, 1}
+	a, b := 0, 1
 	return func() int {
-		n++
-		if n == 0 || n == 1 {
-			return arr[n-1]
-		}
-		next := arr[n-2] + arr[n-1]
-		arr = append(arr, next)
-		return arr[n-1]
+		res := a
+		a, b = b, a+b
+		return res
 	}
 }
